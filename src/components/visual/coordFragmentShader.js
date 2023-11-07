e47b440fcc9c9cc01f355c9eb11d314f76429eb7
Jake Lee <firstvirtue@gmail.com>
2023-11-08 02:34:16 +0900

    Update shader coord code

--- src/components/visual/coordFragmentShader.js
+++ src/components/visual/coordFragmentShader.js
@@ -1,7 +1,46 @@
-var fragCode = 'precision mediump float;'+
-  'varying vec3 vColor;'+
-  'void main(void) {'+
-      'gl_FragColor = vec4(vColor, 1.);'+
-  '}';
+var fragCode = `
+varying vec2 vUv;
+uniform vec3 diffuse;
+uniform float opacity;
+uniform sampler2D tex;
+
+// #include &lt;common&gt;
+// #include &lt;color_pars_fragment&gt;
+// #include &lt;map_particle_pars_fragment&gt;
+// #include &lt;fog_pars_fragment&gt;
+// #include &lt;logdepthbuf_pars_fragment&gt;
+// #include &lt;clipping_planes_pars_fragment&gt;
+
+void main() {
+
+// #include &lt;clipping_planes_fragment&gt;
+
+vec3 outgoingLight = vec3( 0.0 );
+vec4 diffuseColor = vec4( diffuse, opacity );
+vec3 colorA = vec3(0.912,0.191,0.652);
+
+// #include &lt;logdepthbuf_fragment&gt;
+
+vec4 mapTexel = texture2D( tex, vUv );
+// diffuseColor *= mapTexelToLinear( mapTexel );
+
+
+// #include &lt;color_fragment&gt;
+// #include &lt;alphatest_fragment&gt;
+
+outgoingLight = diffuseColor.rgb;
+gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+
+vec3 color = mapTexel.rgb;
+outgoingLight = color.rgb;
+gl_FragColor = vec4( outgoingLight, 1.0 );
+
+// #include &lt;tonemapping_fragment&gt;
+// #include &lt;encodings_fragment&gt;
+// #include &lt;fog_fragment&gt;
+// #include &lt;premultiplied_alpha_fragment&gt;
+
+}
+  `;
 
   export default fragCode
--- src/components/visual/coordVertexShader.js
+++ src/components/visual/coordVertexShader.js
@@ -1,12 +1,44 @@
 // https://stackoverflow.com/questions/52972221/how-can-i-render-points-with-different-colors
 
-var vertCode = 'attribute vec3 coordinates;'+
-  'attribute vec3 color;'+
-  'varying vec3 vColor;'+
-  'void main(void) {' +
-      ' gl_Position = vec4(coordinates, 1.0);' +
-      'vColor = color;'+
-      'gl_PointSize = 10.0;'+
-  '}';
+var vertCode = `
+varying vec2 vUv;
+uniform float size;
+uniform float scale;
+
+// #include &lt;common&gt;
+// #include &lt;color_pars_vertex&gt;
+// #include &lt;fog_pars_vertex&gt;
+// #include &lt;morphtarget_pars_vertex&gt;
+// #include &lt;logdepthbuf_pars_vertex&gt;
+
+// #include &lt;clipping_planes_pars_vertex&gt;
+void main() {
+    // #include &lt;color_vertex&gt;
+    // #include &lt;begin_vertex&gt;
+    
+    vUv = uv;
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+      
+    // #include &lt;morphtarget_vertex&gt;
+    // #include &lt;project_vertex&gt;
+    
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+    gl_PointSize = 10.0;
+    
+    // #ifdef USE_SIZEATTENUATION
+        // bool isPerspective = isPerspectiveMatrix( projectionMatrix );
+        // if ( isPerspective ) gl_PointSize *= ( scale / - mvPosition.z );
+    // #endif
+
+    // #include &lt;logdepthbuf_vertex&gt;
+    // #include &lt;clipping_planes_vertex&gt;
+    // #include &lt;worldpos_vertex&gt;
+    // #include &lt;fog_vertex&gt;
+}
+`;
 
   export default vertCode
