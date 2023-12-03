d79e25a8d7dcfe7aa315722b3ca263b70b9d1fe7
Jake Lee <firstvirtue@gmail.com>
2023-12-03 17:33:58 +0900

    Update square to circle

--- src/components/fboBox/fragmentShader.js
+++ src/components/fboBox/fragmentShader.js
@@ -9,10 +9,24 @@ void main() {
   // vec3 color = vec3(0.34, 0.53, 0.96);
   // gl_FragColor = vec4(color, 1.0);
 
+  // square to circle
+  float r = 0.0, delta = 0.0, alpha = 1.0;
+  vec2 cxy = 2.0 * gl_PointCoord - 1.0;
+  r = dot(cxy, cxy);
+  #ifdef GL_OES_standard_derivatives
+    delta = fwidth(r);
+    alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);
+  #endif
+
+  if (r > 1.0) {
+    discard;
+  }
+
   vec3 outgoingLight = vec3( 0.0 );
   vec3 color = mapTexel.rgb;
   outgoingLight = color.rgb;
-  gl_FragColor = vec4( outgoingLight, 1.0 );
+
+  gl_FragColor = vec4( outgoingLight, 1.0 ) * alpha;
 }
 `
 
--- src/components/fboCurl/fragmentShader.js
+++ src/components/fboCurl/fragmentShader.js
@@ -14,7 +14,21 @@ void main() {
   vec3 outgoingLight = vec3( 0.0 );
   vec3 color = mapTexel.rgb;
   outgoingLight = color.rgb;
-  gl_FragColor = vec4( outgoingLight, 1.0 );
+
+  // square to circle
+  float r = 0.0, delta = 0.0, alpha = 1.0;
+  vec2 cxy = 2.0 * gl_PointCoord - 1.0;
+  r = dot(cxy, cxy);
+  #ifdef GL_OES_standard_derivatives
+    delta = fwidth(r);
+    alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);
+  #endif
+
+  if (r > 1.0) {
+    discard;
+  }
+
+  gl_FragColor = vec4( outgoingLight, 1.0 ) * alpha;
 }
 `
 
